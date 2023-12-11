5ebcb88ee25de2d7d7d88a5b41c80fcb764cb593
Javier Fernandez-Ivern <ivern@netflix.com>
2023-12-11 10:33:40 -0600

    Day 11 solutions

---
+++ src/Puzzle22.java
@@ -0,0 +1,68 @@
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static java.lang.Math.abs;
+
+@SuppressWarnings("unused")
+public class Puzzle22 {
+
+    public BigInteger solve() throws IOException {
+        var lines = Files.readAllLines(Paths.get("./data/day11.txt"));
+
+        int numRows = lines.size();
+        int numCols = lines.get(0).length();
+        Set<Integer> emptyCols = new HashSet<>();
+
+        for (int col = 0; col < numCols; ++col) {
+            boolean empty = true;
+            for (int row = 0; row < numRows; ++row) {
+                if (lines.get(row).charAt(col) == '#') {
+                    empty = false;
+                }
+            }
+            if (empty) {
+                emptyCols.add(col);
+            }
+        }
+
+        int y = 0;
+
+        List<Coordinate> galaxyPositions = new ArrayList<>();
+
+        for (int row = 0; row < numRows; ++row) {
+            int x = 0;
+            boolean empty = true;
+            for (int col = 0; col < numCols; ++col) {
+                if (lines.get(row).charAt(col) == '#') {
+                    empty = false;
+                    galaxyPositions.add(new Coordinate(x, y));
+                }
+                x += emptyCols.contains(col) ? 1_000_000 : 1;
+            }
+            y += empty ? 1_000_000 : 1;
+        }
+
+        BigInteger sum = BigInteger.ZERO;
+        for (int i = 0; i < galaxyPositions.size(); ++i) {
+            var g = galaxyPositions.get(i);
+            for (int j = i + 1; j < galaxyPositions.size(); ++j) {
+                sum = sum.add(BigInteger.valueOf(g.distance(galaxyPositions.get(j))));
+            }
+        }
+
+        return sum;
+    }
+
+    record Coordinate(int x, int y) {
+        int distance(Coordinate c) {
+            return abs(x - c.x) + abs(y - c.y);
+        }
+    }
+
+}
